612a5fd5203aeb3d8caec6cd702073455e25e6f1
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-28 02:00:26 +1300

    revert: "feat: add comments"
    This reverts commit e9766c08c2f41a91779fe54aea5e19fc08e91c85.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 import type { Site, SocialObjects } from "./types";
-import type { GiscusProps } from "@giscus/react";
 
 export const SITE: Site = {
   website: "https://www.zenoix.com",
@@ -154,17 +153,3 @@ export const SOCIALS: SocialObjects = [
     active: false,
   },
 ];
-
-export const GISCUS: GiscusProps = {
-  repo: "zenoix/zenoix-blog",
-  repoId: "R_kgDONGeS9Q",
-  category: "Announcements",
-  categoryId: "DIC_kwDONGeS9c4Cju7C",
-  mapping: "pathname",
-  strict: "0",
-  reactionsEnabled: "0",
-  emitMetadata: "0",
-  inputPosition: "top",
-  lang: "en",
-  loading: "lazy",
-};
