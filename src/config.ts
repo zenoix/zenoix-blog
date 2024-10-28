e5930aca800935ac168f73f1ae132e2e82f13245
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-28 23:23:23 +1300

    chore: set langTag to en-NZ

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ export const SITE: Site = {
 
 export const LOCALE = {
     lang: "en", // html lang code. Set this empty and default will be "en"
-    langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+    langTag: ["en-NZ"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
