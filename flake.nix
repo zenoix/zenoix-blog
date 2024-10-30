25ecad7f15352878d30cf79f588514fbb3d82bd7
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-30 21:11:10 +1300

    chore: switch to devenv

--- flake.nix
+++ flake.nix
@@ -1,27 +1,61 @@
 {
-  description = "Zenoix Blog Dev Environment";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
     let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      # importing package example
-      # packages."x86_64-linux".default = 
-      #   pkgs.callPackage (import ./default.nix) {};
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
 
-      devShells."x86_64-linux".default = pkgs.mkShell {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = [ ];
 
-        packages = with pkgs; [
-          nodejs
-          pnpm
-        ];
+                languages = {
+                  javascript = {
+                    enable = true;
+                    pnpm = {
+                      enable = true;
+                      install.enable = true;
+                    };
+                  };
+                  typescript.enable = true;
+                };
 
-      };
+                # enterShell = '''';
+              }
+            ];
+          };
+        }
+      );
     };
 }
