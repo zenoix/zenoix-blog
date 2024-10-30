3bc404b53c2eaaa29cace55c0469df31f79c2cd8
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-30 22:24:13 +1300

    feat: change font to fira code (text) and jetbrainsmono (code)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -60,7 +60,7 @@ module.exports = {
                 }
             },
             fontFamily: {
-                mono: ["IBM Plex Mono", "monospace"],
+                mono: ["Fira Code", "monospace"],
             },
 
             typography: {
